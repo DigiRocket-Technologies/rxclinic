5cda53d4ce1c6c08d54094d04e2b9dd17ed966ef
Gagandeep-Sethi <107262250+Gagandeep-Sethi@users.noreply.github.com>
2025-04-02 15:51:46 +0530

    Update server.js for cors

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 // Enable CORS for your frontend origin
 app.use(
   cors({
-    origin: "http://localhost:8080", // Allow requests from this origin
+    origin: ["http://localhost:8080","https://rxclinic.vercel.app"], // Allow requests from this origin
     methods: ["POST"], // Allow only POST requests (or add others like GET if needed)
     allowedHeaders: ["Content-Type"], // Allow Content-Type header
   })
