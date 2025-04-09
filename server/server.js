9aec2051aa841170666d662f1ef1ee4e55bc704e
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2025-04-09 20:09:36 +0530

    new route for vaccine form submission

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors"; // Import cors
-import { submitForm } from "./controllers/formController.js";
+import { submitForm, submitVaccineForm } from "./controllers/formController.js";
 
 const app = express();
 
 // Enable CORS for your frontend origin
 app.use(
   cors({
-    origin: ["http://localhost:8080","https://rxclinic.vercel.app"], // Allow requests from this origin
+    origin: ["http://localhost:8080", "https://rxclinic.vercel.app"], // Allow requests from this origin
     methods: ["POST"], // Allow only POST requests (or add others like GET if needed)
     allowedHeaders: ["Content-Type"], // Allow Content-Type header
   })
@@ -17,6 +17,11 @@ app.use(
 app.use(express.json());
 
 // Route to handle form submission
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+
 app.post("/submitForm", submitForm);
+app.post("/submit-vaccine-form", submitVaccineForm);
 
 app.listen(3000, () => console.log(`Server listening at port 3000`));
