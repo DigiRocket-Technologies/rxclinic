08a25b0ed9a7ea05c0fa420655a527c38ce7fbb5
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2025-04-09 20:08:03 +0530

    to send request to backend to submit vaccine form

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -20,3 +20,26 @@ export const submitFormData = async (formData) => {
     throw error; // Re-throw to handle in the calling code
   }
 };
+
+export const submitVaccineForm = async (formData) => {
+  try {
+    const response = await fetch("http://localhost:3000/submit-vaccine-form", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log("Form submission successful:", result);
+    return result; // e.g., { message: "Form submitted successfully, email sent" }
+  } catch (error) {
+    console.error("Error submitting form:", error.message);
+    throw error; // Re-throw to handle in the calling code
+  }
+};
