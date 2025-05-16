30237dbfd4e6ec487e79bc3d1e8a53e1b8256501
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2025-05-16 19:17:37 +0530

    send consultion form to backend

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -43,3 +43,29 @@ export const submitVaccineForm = async (formData) => {
     throw error; // Re-throw to handle in the calling code
   }
 };
+
+export const submitConsultationForm = async (formData) => {
+  try {
+    const response = await fetch(
+      "http://localhost:3000/submit-consultation-Form",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log("Form submission successful:", result);
+    return result; // e.g., { message: "Form submitted successfully, email sent" }
+  } catch (error) {
+    console.error("Error submitting form:", error.message);
+    throw error; // Re-throw to handle in the calling code
+  }
+};
